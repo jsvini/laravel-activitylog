aad028f704887883d37d192cacbb228b9318b310
Vinícius Silva <github@kurai.in>
2017-03-03 19:31:59 -0300

    Update helpers.php
    Fix php7 bull

--- src/helpers.php
+++ src/helpers.php
@@ -3,10 +3,12 @@
 use Spatie\Activitylog\ActivityLogger;
 
 if (! function_exists('activity')) {
-    function activity(string $logName = null): ActivityLogger
+    /**
+     * @param string $logName
+     */
+    function activity($logName = null)
     {
         $defaultLogName = config('laravel-activitylog.default_log_name');
-
-        return app(ActivityLogger::class)->useLog($logName ?? $defaultLogName);
+        return app(ActivityLogger::class)->useLog($logName != null ? $logName : $defaultLogName);
     }
 }
